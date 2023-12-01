7d7ae69b7d4d02d7a81ed377228e3ba87b8ba9fe
WyattGotter <75274320+WyattGotter@users.noreply.github.com>
2023-12-01 09:04:47 -0600

    Add files via upload

--- 303assignmentfinal/303final.js
+++ 303assignmentfinal/303final.js
@@ -36,14 +36,26 @@ let paddle2 = {
 
 let keys = { w: false, s: false };
 
+let powerUpTypes = ['multiball', 'breakout'];
+let currentPowerUpTypeIndex = 0;
+let lastScore = 0;
+
 let powerUp = {
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     width: 58,
     height: 58,
-    visible: true
+    visible: true,
+    type: powerUpTypes[currentPowerUpTypeIndex],
+    move() {
+        this.x += Math.random() * 5 - 2.5; // Random horizontal movement
+        this.y += Math.random() * 5 - 2.5; // Random vertical movement
+        // Keep the power-up within the canvas
+        this.x = Math.max(0, Math.min(this.x, canvas.width - this.width));
+        this.y = Math.max(0, Math.min(this.y, canvas.height - this.height));
+    }
 };
-
+let breakoutBlocks = [];
 let additionalBalls = [];
 
 // Event listeners for keydown and keyup
@@ -70,15 +82,27 @@ function movePaddles() {
         paddle1.y += paddle1.speed;
     }
 
-    let paddle2Center = paddle2.y + paddle2.height / 2;
-    if (Math.random() < aiReactionThreshold) {
-        if (ball.y > paddle2Center && paddle2.y + paddle2.height < canvas.height) {
-            paddle2.y += Math.min(aiMaxSpeed, paddle2.speed);
-        } else if (ball.y < paddle2Center && paddle2.y > 0) {
-            paddle2.y -= Math.min(aiMaxSpeed, paddle2.speed);
+    // Determine which ball to track
+    let ballToTrack = ball;
+    if (!ball.visible && additionalBalls.length > 0) {
+        // Find the first visible additional ball
+        ballToTrack = additionalBalls.find(b => b.visible);
+    }
+
+    if (ballToTrack) {
+        // AI paddle movement based on the ball to track
+        let paddle2Center = paddle2.y + paddle2.height / 2;
+        let deltaY = ballToTrack.y - paddle2Center;
+        if (Math.abs(deltaY) > aiReactionThreshold) {
+            if (deltaY > 0 && paddle2.y + paddle2.height < canvas.height) {
+                paddle2.y += Math.min(Math.abs(deltaY), aiMaxSpeed);
+            } else if (deltaY < 0 && paddle2.y > 0) {
+                paddle2.y -= Math.min(Math.abs(deltaY), aiMaxSpeed);
+            }
         }
     }
 }
+
 // Load and play background music
 backgroundMusic.loop = true;
 backgroundMusic.play();
@@ -93,7 +117,7 @@ function drawBall(ballObj) {
 
 function drawPowerUp() {
     if (!powerUp.visible) return;
-    ctx.fillStyle = '#FFD700'; // Gold color
+    ctx.fillStyle = powerUp.type === 'multiball' ? '#FFD700' : '#FF6347'; // Gold for multiball, Tomato for breakout
     ctx.fillRect(powerUp.x, powerUp.y, powerUp.width, powerUp.height);
 }
 
@@ -111,20 +135,32 @@ function updatePaddleColor(paddle) {
 }
 
 function spawnAdditionalBall() {
-    let newBall = {
+    // Create the first additional ball
+    let newBall1 = {
         x: powerUp.x, y: powerUp.y,
         radius: ball.radius,
         speedX: -ball.speedX, speedY: -ball.speedY,
         visible: true
     };
-    additionalBalls.push(newBall);
-}
 
+    // Create the second additional ball with slightly different speeds
+    let newBall2 = {
+        x: powerUp.x, y: powerUp.y,
+        radius: ball.radius,
+        speedX: -ball.speedX * 1.1, speedY: -ball.speedY * 0.9, // Adjust speeds for variety
+        visible: true
+    };
+
+    additionalBalls.push(newBall1, newBall2); // Add both balls to the additionalBalls array
+}
 function moveBall(ballObj) {
     if (!ballObj.visible) return;
 
     ballObj.x += ballObj.speedX;
     ballObj.y += ballObj.speedY;
+
+    // Handle collisions with breakout blocks
+    handleBallCollisionWithBreakoutBlocks(ballObj);
 
     // Collision with top and bottom
     if (ballObj.y + ballObj.radius > canvas.height || ballObj.y - ballObj.radius < 0) {
@@ -134,11 +170,11 @@ function moveBall(ballObj) {
     // Collision with paddles
     if (ballObj.x - ballObj.radius < paddle1.x + paddle1.width && ballObj.y > paddle1.y && ballObj.y < paddle1.y + paddle1.height) {
         ballObj.speedX = -ballObj.speedX;
-        collisionSound.play(); // Play collision sound
+        collisionSound.play(); // Play collision sound only for paddle collision
     }
     if (ballObj.x + ballObj.radius > paddle2.x && ballObj.y > paddle2.y && ballObj.y < paddle2.y + paddle2.height) {
         ballObj.speedX = -ballObj.speedX;
-        collisionSound.play(); // Play collision sound
+        collisionSound.play(); // Play collision sound only for paddle collision
     }
 
     // Collision with power-up
@@ -146,10 +182,15 @@ function moveBall(ballObj) {
         ballObj.y + ballObj.radius > powerUp.y && ballObj.y - ballObj.radius < powerUp.y + powerUp.height && 
         powerUp.visible) {
         powerUp.visible = false; // Hide the power-up
-        spawnAdditionalBall();
+        if (powerUp.type === 'multiball') {
+            spawnAdditionalBall();
+        } else if (powerUp.type === 'breakout') {
+            spawnBreakoutBlocks();
+        }
+        
     }
 
-    // Check if the ball goes past the paddles
+    // Check if the ball goes past the paddles (Moved outside the power-up collision if-statement)
     if (ballObj.x + ballObj.radius < 0 || ballObj.x - ballObj.radius > canvas.width) {
         ballObj.visible = false;
         updateScore(ballObj);
@@ -170,10 +211,58 @@ function updateScore(ballObj) {
         paddle1.score++;
     }
 
+    if (paddle1.score + paddle2.score > lastScore + 2) {
+        lastScore = paddle1.score + paddle2.score;
+        currentPowerUpTypeIndex = (currentPowerUpTypeIndex + 1) % powerUpTypes.length;
+        powerUp.type = powerUpTypes[currentPowerUpTypeIndex];
+        powerUp.visible = true; // Respawn the power-up
+        powerUp.x = Math.random() * canvas.width;
+        powerUp.y = Math.random() * canvas.height;
+    }
+
     // Check if all balls are no longer visible
     if (!ball.visible && additionalBalls.every(b => !b.visible)) {
         resetGame();
     }
+}
+function spawnBreakoutBlocks(opponentPaddle) {
+    // Calculate the size and number of blocks to cover a larger surface area
+    const blockWidth = canvas.width * 0.05; // Blocks are 5% of the canvas width
+    const blockHeight = canvas.height * 0.05; // Blocks are 5% of the canvas height
+    const rows = Math.floor(canvas.height * 0.6 / blockHeight); // Covers 60% of the canvas height
+    const cols = 1; // Single column of large blocks
+    const offsetX = opponentPaddle === paddle1 ? paddle1.x + paddle1.width : paddle2.x - blockWidth; // Place in front of the opponent paddle
+    
+    breakoutBlocks = []; // Clear existing blocks
+    for (let r = 0; r < rows; r++) {
+        breakoutBlocks.push({
+            x: offsetX,
+            y: (canvas.height - rows * blockHeight) / 2 + r * blockHeight, // Center vertically
+            width: blockWidth,
+            height: blockHeight,
+            visible: true
+        });
+    }
+}
+
+function drawBreakoutBlocks() {
+    breakoutBlocks.forEach(block => {
+        if (block.visible) {
+            ctx.fillStyle = '#FF6347'; // Tomato color for the breakout blocks
+            ctx.fillRect(block.x, block.y, block.width, block.height);
+        }
+    });
+}
+
+function handleBallCollisionWithBreakoutBlocks(ballObj) {
+    breakoutBlocks.forEach(block => {
+        if (block.visible && ballObj.x + ballObj.radius > block.x && ballObj.x - ballObj.radius < block.x + block.width &&
+            ballObj.y + ballObj.radius > block.y && ballObj.y - ballObj.radius < block.y + block.height) {
+            ballObj.speedX = -ballObj.speedX; // Reflect the ball's X direction
+            block.visible = false; // Hide the block
+            collisionSound.play(); // Play collision sound
+        }
+    });
 }
 
 function resetGame() {
@@ -194,24 +283,38 @@ enableAudioButton.addEventListener('click', function() {
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Move and draw power-up
+    powerUp.move();
+    drawPowerUp();
+
+    // Move all balls
     moveBall(ball);
     additionalBalls.forEach(moveBall);
 
+    // Draw all balls
     drawBall(ball);
-    additionalBalls.forEach(ball => drawBall(ball));
+    additionalBalls.forEach(drawBall);
 
+    // Draw paddles and update their colors
     drawPaddle(paddle1);
     drawPaddle(paddle2);
-
     updatePaddleColor(paddle1);
     updatePaddleColor(paddle2);
 
+    // Move paddles based on key presses
     movePaddles();
 
+    // Draw the breakout blocks
+    drawBreakoutBlocks();
+    
+    handleBallCollisionWithBreakoutBlocks(ball); // Handle collisions for the main ball
+    additionalBalls.forEach(handleBallCollisionWithBreakoutBlocks); // Handle collisions for additional balls
+    // Draw the score
     drawScore();
-    drawPowerUp();
 
+    // Request the next animation frame
     requestAnimationFrame(gameLoop);
 }
+
 
 gameLoop();
